c4b21f82585ed42dfe2162242979f40a5a41a9ff
Илья <camypai.13@list.ru>
2019-01-10 16:00:29 +0300

    Complete homework7

---
+++ Homework7/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace Homework7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiNewGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tsmiExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.правилаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblHelp = new System.Windows.Forms.Label();
+            this.tbHistory = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.справкаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(654, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiNewGame,
+            this.toolStripMenuItem1,
+            this.tsmiExit});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // tsmiNewGame
+            // 
+            this.tsmiNewGame.Name = "tsmiNewGame";
+            this.tsmiNewGame.Size = new System.Drawing.Size(136, 22);
+            this.tsmiNewGame.Text = "Новая игра";
+            this.tsmiNewGame.Click += new System.EventHandler(this.tsmiNewGame_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(133, 6);
+            // 
+            // tsmiExit
+            // 
+            this.tsmiExit.Name = "tsmiExit";
+            this.tsmiExit.Size = new System.Drawing.Size(136, 22);
+            this.tsmiExit.Text = "Выход";
+            this.tsmiExit.Click += new System.EventHandler(this.tsmiExit_Click);
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmiAbout,
+            this.правилаToolStripMenuItem});
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            // 
+            // tsmiAbout
+            // 
+            this.tsmiAbout.Name = "tsmiAbout";
+            this.tsmiAbout.Size = new System.Drawing.Size(149, 22);
+            this.tsmiAbout.Text = "О программе";
+            this.tsmiAbout.Click += new System.EventHandler(this.tsmiAbout_Click);
+            // 
+            // правилаToolStripMenuItem
+            // 
+            this.правилаToolStripMenuItem.Name = "правилаToolStripMenuItem";
+            this.правилаToolStripMenuItem.Size = new System.Drawing.Size(149, 22);
+            this.правилаToolStripMenuItem.Text = "Правила";
+            this.правилаToolStripMenuItem.Click += new System.EventHandler(this.правилаToolStripMenuItem_Click);
+            // 
+            // lblHelp
+            // 
+            this.lblHelp.AutoSize = true;
+            this.lblHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblHelp.Location = new System.Drawing.Point(118, 66);
+            this.lblHelp.Name = "lblHelp";
+            this.lblHelp.Size = new System.Drawing.Size(527, 39);
+            this.lblHelp.TabIndex = 2;
+            this.lblHelp.Text = "Угадайте число, что я загадал";
+            this.lblHelp.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.lblHelp.UseMnemonic = false;
+            // 
+            // tbHistory
+            // 
+            this.tbHistory.Location = new System.Drawing.Point(12, 27);
+            this.tbHistory.Multiline = true;
+            this.tbHistory.Name = "tbHistory";
+            this.tbHistory.ReadOnly = true;
+            this.tbHistory.Size = new System.Drawing.Size(100, 248);
+            this.tbHistory.TabIndex = 3;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStart.Location = new System.Drawing.Point(325, 115);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(124, 69);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Начать";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(654, 287);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbHistory);
+            this.Controls.Add(this.lblHelp);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiNewGame;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem tsmiExit;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tsmiAbout;
+        public System.Windows.Forms.Label lblHelp;
+        public System.Windows.Forms.TextBox tbHistory;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ToolStripMenuItem правилаToolStripMenuItem;
+    }
+}
+
---
+++ Homework7/Game.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7
+{
+    public class Game
+    {
+        public int Number { get; set; }
+        public Stopwatch Stopwatch { get; set; }
+        public int LastNumber { get; set; }
+
+        public int NumbersHistoryCount { get; private set; }
+//        private readonly List<int> NumbersHistory;
+
+        public enum CheckSate
+        {
+            Greate = 1,
+            Less = -1,
+            Equal = 0
+        }
+
+        public Game()
+        {
+            Number = new Random().Next(1, 101);
+            Stopwatch = Stopwatch.StartNew();
+//            NumbersHistory = new List<int>();
+        }
+
+        public CheckSate CheckUserInput(int userNumber)
+        {
+            LastNumber = userNumber;
+            NumbersHistoryCount++;
+//            NumbersHistory.Add(LastNumber);
+
+            if (userNumber > Number)
+            {
+                return CheckSate.Greate;
+            }
+
+            if (userNumber < Number)
+            {
+                return CheckSate.Less;
+            }
+
+            Stopwatch.Stop();
+            return CheckSate.Equal;
+
+//            Update?.Invoke();
+        }
+    }
+}
