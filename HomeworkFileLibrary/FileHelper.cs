cb0414e06dc1f1f54d9667e940f1c5ac1fd824cf
Илья <camypai.13@list.ru>
2019-01-17 18:58:57 +0300

    Added File helper

---
+++ HomeworkFileLibrary/FileHelper.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Text;
+
+namespace HomeworkFileLibrary
+{
+    public static class FileHelper
+    {
+        public static bool WriteFile(string path, string data)
+        {
+            try
+            {
+                using (var sw = new StreamWriter(path, false))
+                {
+                    sw.Write(data);
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static string GetFile(string path)
+        {
+            try
+            {
+                using (var sr = new StreamReader(path, Encoding.Default))
+                {
+                    return sr.ReadToEnd();
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
